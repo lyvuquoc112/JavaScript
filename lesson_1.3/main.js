604181a4cf619996a4719dc62c29e68f63f049b1
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-05 21:29:10 +0700

    Arthimetic

---
+++ lesson_1.3/main.js
@@ -0,0 +1,35 @@
+/**
+ * Toán tử số học
+ * + --> Phép cộng
+ * - --> Phép trừ
+ * * --> Phép nhân
+ * ** --> Phép lũy thừa
+ * / --> Phép chia
+ * % --> Phép chia lấy dư
+ * ++ --> Tăng 1 giá trị số
+ * -- --> Giảm 1 giá trị số
+ */
+
+var a = 8;
+var b = 2;
+
+var c = a + b;
+console.log(c);
+
+var c = a - b;
+console.log(c);
+
+var c = a ** b;
+console.log(c);
+
+var c = a / b;
+console.log(c);
+
+var c = a % b;
+console.log(c);
+
+var c = ++a;
+console.log(c);
+
+var c = --a;
+console.log(c);
