2554fc505fe84b4da3d6c8723cbde441f03fa8b7
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-05 20:45:38 +0700

    Khởi tạo biến và gán giá trị

---
+++ lesson_1/main.js
@@ -0,0 +1,7 @@
+// alert("Hello, world!");
+// có ; cuối dòng lệnh hay không đều được
+var fullName = 'Ly Vu Quoc Huy'; // tạo biến và gán giá trị
+var age = 21;
+// trong mỗi lần thì chỉ có 1 lệnh alert được hoạt động
+alert(fullName);
+alert(age);
