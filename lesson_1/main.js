11903c9eca39c4dd37c7b5fea3d164c2763711b1
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-05 20:54:15 +0700

    Sử dụng comment

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,7 +1,25 @@
 // alert("Hello, world!");
 // có ; cuối dòng lệnh hay không đều được
+// vùng khai báo
 var fullName = 'Ly Vu Quoc Huy'; // tạo biến và gán giá trị
 var age = 21;
 // trong mỗi lần thì chỉ có 1 lệnh alert được hoạt động
+// gọi hàm thông báo
 alert(fullName);
 alert(age);
+
+/** 
+ * Comments trong JS
+ * 
+ * 1. Mục đích sử dụng
+ * - ghi chú
+ * - vô hiệu hóa mã
+ * 
+ * 2. Loại comments
+ * - Comment 1 dòng: //
+ * - Comment nhiều dòng: /* ... *
+ * 
+ * 3. Sử dụng phím tắt
+ * - Windows: Ctrl + /
+ * - Mac: Cmd + /
+*/
