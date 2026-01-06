a9e04e7421d601f2bb4b3e72e487fd4bc2acab84
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-06 08:52:53 +0700

    Comparison

---
+++ lesson_1.7/main.js
@@ -0,0 +1,26 @@
+/**
+ * Toán tử so sánh
+ * Toán tử
+ * ==       -> bằng
+ * !=       -> không bằng
+ * >        -> Lớn hơn
+ * <        -> Nhỏ hơn
+ * >=       -> lớn hơn hoặc bằng
+ * <=       -> nhỏ hơn hoặc bằng
+ */
+
+var a = 2;
+var b = 2;
+if (a <= b) {
+  console.log("Dieu kien dung!");
+} else {
+  console.log("Dieu kien sai!");
+}
+
+var a = 'Nguyen Van A';
+var b = 'Nguyen Van a';
+if (a == b) {
+  console.log("Dieu kien dung!");
+} else {
+  console.log("Dieu kien sai!");
+}
