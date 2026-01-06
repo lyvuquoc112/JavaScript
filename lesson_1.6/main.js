f605805445287777d1c013eb5d9b0e31aa17e003
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-06 08:43:36 +0700

    String operation

---
+++ lesson_1.6/main.js
@@ -0,0 +1,13 @@
+/**
+ * Toán tử chuỗi - String operator
+ */
+
+// var firstName = "Lý";
+// var lastName = "Huy";
+// console.log(firstName + " " + lastName);
+
+var name = "Ly";
+// name = name + " Huy";
+name += " Huy";
+console.log("name: ", name);
+
