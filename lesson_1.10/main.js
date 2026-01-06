b10b962b2c694d5ef07d61ac774af89879b713b8
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-06 09:16:47 +0700

    Logical

---
+++ lesson_1.10/main.js
@@ -0,0 +1,27 @@
+/**
+ * Toán tử logical
+ * 1. && - And
+ * 2. || - Or
+ * 3. !- Not
+ */
+
+var a = 1;
+var b = 2;
+var c = 3;
+// if (a > 0 && b > 0 && c > 0) {
+//   console.log("DIEU KIEN DUNG!");
+// }else{
+//     console.log("DIEU KIEN SAI!");
+// }
+
+if (a < 0 || b < 0 || c > 0) {
+  console.log("DIEU KIEN DUNG!");
+}else{
+    console.log("DIEU KIEN SAI!");
+}
+
+if (!(a < 0)) {
+  console.log("DIEU KIEN DUNG!");
+}else{
+    console.log("DIEU KIEN SAI!");
+}
