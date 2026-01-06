59d239c5c50000cb18b534c02ec5d6be7527da67
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-06 09:06:21 +0700

    If-eles

---
+++ lesson_1.9/main.js
@@ -0,0 +1,36 @@
+/**
+ * If - eles
+ */
+
+
+
+// if(false){
+//     console.log('Dieu kien dung!')
+// }else{
+//     console.log('Dieu kien sai!')
+// }
+
+
+// var isSuccess = 1 < 2;
+
+// if(isSuccess){
+//     console.log('Dieu kien dung!')
+// }else{
+//     console.log('Dieu kien sai!')
+// }
+
+/** trong javasript co 6 du lieu conert sang boolean la false, con lai la true
+ * 0
+ * false
+ * '' - ""
+ * undefined
+ * NaN
+ * null
+ */
+// var fullName = 'Nguyen Van A';
+var fullName ='';
+if(fullName){// nguyen van A chuyen thanh true
+    console.log('Dieu kien dung!')
+}else{
+    console.log('Dieu kien sai!')
+}
