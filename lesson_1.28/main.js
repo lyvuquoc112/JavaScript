2a5ca8bb6b7fed10d9e7e849b2089aa2bc602f30
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-07 17:16:14 +0700

    Update them gacha

--- lesson_1.28/main.js
+++ lesson_1.28/main.js
@@ -27,6 +27,47 @@ console.log(
 
 var randomNumber = [1, 2, 3, 4];
 function getRandomItem(randomNumber) {
-  return randomNumber[Math.floor(Math.random() * randomNumber.lenght())];
+  return randomNumber[Math.floor(Math.random() * randomNumber.length)];
 }
-console.log(getRandomItem(randomNumber))
+console.log(getRandomItem(randomNumber));
+
+// quay gach
+// có 3 loại
+// character 3 star
+// character 4 star
+// character 5 star
+// quay 10 lần
+
+function percentageOfEachPush() {
+  return parseFloat((Math.random() * 100).toFixed(1));
+}
+
+function getRandomCharacter(categoryCharaters) {
+  return categoryCharaters[
+    Math.floor(Math.random() * categoryCharaters.length)
+  ];
+}
+
+const character5Star = ["A", "B", "C", "D"];
+const character4Star = ["a", "b", "c", "d", "e", "f", "g"];
+const character3Star = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const listCharactersGet = [];
+console.log("--- Bắt đầu Gacha 10 lần ---");
+for (let i = 0; i < 10; ++i) {
+  let character;
+  let chance = percentageOfEachPush();
+  if (chance <= 0.1) {
+    character = getRandomCharacter(character5Star);
+    console.log(`lần ${i+1}: ${chance}\%\ lấy được tướng ${character} 5 sao`)
+  }
+  else if (chance > 0.1 && chance <= 5) {
+    character = getRandomCharacter(character4Star);
+    console.log(`lần ${i+1}: ${chance}% lấy được tướng ${character} 4 sao`)
+  }
+  else {
+    character = getRandomCharacter(character3Star);
+    console.log(`lần ${i+1}: ${chance}% lấy được tướng ${character} 3 sao`)
+  }
+  listCharactersGet.push(character);
+}
+console.log(listCharactersGet);
