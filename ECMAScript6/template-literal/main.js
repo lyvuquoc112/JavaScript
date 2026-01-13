2685999cfc61c902ee01d961d6e5552883749755
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-13 15:36:58 +0700

    Template string

---
+++ ECMAScript6/template-literal/main.js
@@ -0,0 +1,15 @@
+//2. template literals
+//3. Multi-line String
+const courseName = "JavaScript";
+const description = "Course name: " + courseName;
+const description1 = `Course name: ${courseName}`;
+const description2 = `Day la dau noi suy => \${}`;
+console.log(description);
+console.log(description1);
+console.log(description2);
+const Lines = "Line 1\nLine 2\nLine 3";
+console.log(Lines);
+const lines1 = `Line 1
+Line 2
+Line 3`;
+console.log(lines1);
