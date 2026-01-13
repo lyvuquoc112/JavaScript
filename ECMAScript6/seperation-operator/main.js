af8a17668781edb615bb421d264f1ff7f16e8b9c
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-01-13 17:08:55 +0700

    spread

---
+++ ECMAScript6/seperation-operator/main.js
@@ -0,0 +1,24 @@
+// dùng để nối mảng
+// var array = ["JavaScript", "b", "c"];
+// var array2 = [4, 5, 6, 7];
+// var array3 = [...array2, ...array];
+// console.log(array3);
+
+// dùng để nối object
+// var obj1 = { name: "JavaScript" };
+// var obj2 = { price: 1000 };
+// var obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+// Cùng key thì thằng sau ghi đè thằng trước
+// var config1 = {
+//   name: "a",
+//   age: "b",
+//   other: "c",
+// };
+// var config2 = {
+//   place: "e",
+//   ...config1,
+//   name: "d", // cùng key thì thằng sau sẽ ghi đè thằng trước
+// };
+// console.log(config2);
